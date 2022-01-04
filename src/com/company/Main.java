8a13f63aa6a13cc9db18ba254cc59ee0487ae197
BohdanShchehliuk <shbp87@gmail.com>
2022-01-04 15:33:32 +0200

    finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (long[] tm : removNb(26)
+        ) {
+            System.out.print(Arrays.toString(tm) + " ");
+        }
+        ;
+    }
+
+    public static List<long[]> removNb(long n) {
+        List<long[]> list = new ArrayList<>();
+
+        long s = n * (1 + n) / 2;
+
+long b;
+        for (long i = n/2; i <= n; i++) {
+
+            b = (s-i)/(i+1);
+            if (s - i-b==i*b){
+                list.add(new long[]{i, b});
+
+            }
+
+        }
+        return list;
     }
 }
